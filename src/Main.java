28640380b2cc08486f7a32a2e3d2369cc7478d73
Hoad3r <detup.4@gmail.com>
2023-03-22 14:47:14 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um numero");
+        double numero = scanner.nextInt();
+
+        if(numero %2==0){//par
+            System.out.println("A raiz cúbica de "+numero+" é "+Math.cbrt(numero)); //cbrt cubica
+        }else{
+            System.out.println("A raiz quadrada de "+numero+" é "+Math.sqrt(numero));
+        }
+
+    }
+}
