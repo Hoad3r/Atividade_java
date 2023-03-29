7f7002412528fedbd0b9bb0892f6b1fc72df2843
Hoad3r <128629346+Hoad3r@users.noreply.github.com>
2023-03-29 17:05:45 -0300

    Update Questao01.java

--- src/Questao01.java
+++ src/Questao01.java
@@ -5,44 +5,48 @@ public class Questao01 {
         Scanner scanner = new Scanner(System.in);
         System.out.println(
                 "Seja bem-vindo a sua calculadora\n [1] Soma \n [2] Subtração \n [3] Divisão \n [4] Multiplicação \n [5] Potenciação \n [6] Raiz Cúbica  \n [7] Raiz Quadrada ");
-        int opção = scanner.nextInt();
-        if (opção < 5) {
+        int opcao = scanner.nextInt();
+        if (opcao < 5) {
             System.out.println("Digite o numero");
             int x = scanner.nextInt();
             System.out.println("Digite o outro numero");
             int y = scanner.nextInt();
-            switch (opção) {
+            switch (opcao) {
                 case 1: {
                     int soma = x + y;
                     System.out.println(soma);
+                    break;
                 }
                 case 2: {
                     int subtração = x - y;
                     System.out.println(subtração);
+                    break;
                 }
                 case 3: {
                     int divisão = x / y;
                     System.out.println(divisão);
+                    break;
                 }
                 case 4: {
                     int multi = x * y;
                     System.out.println(multi);
+                    break;
                 }
             }
-        } else if (opção >= 5) {
-            if (opção == 5) {
+        } else if (opcao >= 5) {
+            if (opcao == 5) {
                 System.out.println("Digite o numero a ser elevado");
                 int x = scanner.nextInt();
                 System.out.println("Digite a quantidade de vezes a ser elevado");
                 int y = scanner.nextInt();
                 double potencia = Math.pow(x, y);
                 System.out.println(potencia);
-            } else if (opção == 6) {
+            } else if (opcao == 6) {
                 System.out.println("Digite um numero");
                 int x = scanner.nextInt();
                 double raiz_cúbica = Math.cbrt(x);
                 System.out.println(raiz_cúbica);
-            } else if (opção == 7) {
+            } else if (opcao == 7) {
                 System.out.println("Digite um numero");
                 int x = scanner.nextInt();
                 double raiz_quadrada = Math.sqrt(x);
